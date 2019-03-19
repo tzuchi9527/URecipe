58c56cfb1a110c99fb318a919232f2361d2b2692
tzuchi9527 <tzuchilin.0420@gmail.com>
2019-03-18 21:51:02 -0700

    get week step count

--- cs225/app/src/main/java/com/example/apple/urecipe/HomeFragment.java
+++ cs225/app/src/main/java/com/example/apple/urecipe/HomeFragment.java
@@ -3,6 +3,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.service.autofill.Dataset;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
@@ -24,15 +25,26 @@ import com.example.apple.urecipe.db.DatabaseAccess;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataPoint;
 import com.google.android.gms.fitness.data.DataSet;
 import com.google.android.gms.fitness.data.DataType;
 import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.data.Bucket;
+import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.result.DataReadResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-import com.example.apple.urecipe.common.logger.Log;
+// import com.example.apple.urecipe.common.logger.Log;
+import android.util.Log;
 import com.google.android.gms.tasks.Task;
+import java.util.Calendar;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class HomeFragment extends Fragment {
 
@@ -48,8 +60,14 @@ public class HomeFragment extends Fragment {
     private TextView expended_calories_view;
     private TextView step_count_view;
     private TextView user_bmr_view;
+    private TextView week_step_count_view;
 
     private Button add_new_diary;
+
+    public List<DataSet> week_calories_list;
+    public List<DataSet> week_step_count_list;
+    public int week_step_count = 0;
+    public float week_expended_calories = 0;
 
     @Nullable
     @Override
@@ -81,12 +99,13 @@ public class HomeFragment extends Fragment {
         readCaloriesData();
         readStepCountData();
 
-        SharedPreferences sharedPref = getActivity().getSharedPreferences(
-                "com.example.apple.urecipe.user_personal_model", Context.MODE_PRIVATE);
-        user_bmr = sharedPref.getFloat("user_bmr", 0.0f);
+        readStepCountHistoryData();
+        readCaloriesHistoryData();
 
         step_count_view = view.findViewById(R.id.step_count);
         step_count_view.setText("Step Count: " + String.valueOf(step_count));
+        week_step_count_view = view.findViewById(R.id.week_step_count);
+        week_step_count_view.setText("Week Step Count: " + String.valueOf(week_step_count));
         expended_calories_view = view.findViewById(R.id.expanded_calories);
         expended_calories_view.setText("Expended Calories: " + String.valueOf(expended_calories));
         user_bmr_view = view.findViewById(R.id.user_bmr);
@@ -226,5 +245,184 @@ public class HomeFragment extends Fragment {
                                 Log.w(TAG, "There was a problem getting the calories.", e);
                             }
                         });
+    }
+
+    /**
+     * Asynchronous task to read the history data. When the task succeeds, it will print out the data.
+     */
+    private Task<DataReadResponse> readCaloriesHistoryData() {
+        // Begin by creating the query.
+        DataReadRequest readRequest = queryCaloriesData();
+
+        // Invoke the History API to fetch the data with the query
+        return Fitness.getHistoryClient(getActivity(), GoogleSignIn.getLastSignedInAccount(getActivity()))
+                .readData(readRequest)
+                .addOnSuccessListener(
+                        new OnSuccessListener<DataReadResponse>() {
+                            @Override
+                            public void onSuccess(DataReadResponse dataReadResponse) {
+                                // For the sake of the sample, we'll print the data so we can see what we just
+                                // added. In general, logging fitness information should be avoided for privacy
+                                // reasons.
+                                cumulateCalories(dataReadResponse);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e(TAG, "There was a problem reading the data.", e);
+                            }
+                        });
+    }
+
+    /**
+     * Asynchronous task to read the history data. When the task succeeds, it will print out the data.
+     */
+    private Task<DataReadResponse> readStepCountHistoryData() {
+        // Begin by creating the query.
+        DataReadRequest readRequest = queryStepCountData();
+
+        // Invoke the History API to fetch the data with the query
+        return Fitness.getHistoryClient(getActivity(), GoogleSignIn.getLastSignedInAccount(getActivity()))
+                .readData(readRequest)
+                .addOnSuccessListener(
+                        new OnSuccessListener<DataReadResponse>() {
+                            @Override
+                            public void onSuccess(DataReadResponse dataReadResponse) {
+                                // For the sake of the sample, we'll print the data so we can see what we just
+                                // added. In general, logging fitness information should be avoided for privacy
+                                // reasons.
+                                cumulateStepCount(dataReadResponse);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e(TAG, "There was a problem reading the data.", e);
+                            }
+                        });
+    }
+
+
+    /** Returns a {@link DataReadRequest} for all step count changes in the past week. */
+    public static DataReadRequest queryCaloriesData() {
+        // [START build_read_data_request]
+        // Setting a start and end date using a range of 1 week before this moment.
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        long startTime = cal.getTimeInMillis();
+
+        DateFormat dateFormat = DateFormat.getDateInstance();
+        Log.i(TAG, "Range Start: " + dateFormat.format(startTime));
+        Log.i(TAG, "Range End: " + dateFormat.format(endTime));
+
+        DataReadRequest readRequest =
+                new DataReadRequest.Builder()
+                        .aggregate(DataType.TYPE_CALORIES_EXPENDED, DataType.AGGREGATE_CALORIES_EXPENDED)
+                        .bucketByTime(1, TimeUnit.DAYS)
+                        .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                        .build();
+        // [END build_read_data_request]
+
+        return readRequest;
+    }
+
+
+    /** Returns a {@link DataReadRequest} for all step count changes in the past week. */
+    public static DataReadRequest queryStepCountData() {
+        // [START build_read_data_request]
+        // Setting a start and end date using a range of 1 week before this moment.
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        long startTime = cal.getTimeInMillis();
+
+        DateFormat dateFormat = DateFormat.getDateInstance();
+        Log.i(TAG, "Range Start: " + dateFormat.format(startTime));
+        Log.i(TAG, "Range End: " + dateFormat.format(endTime));
+
+        DataReadRequest readRequest =
+                new DataReadRequest.Builder()
+                        .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                        .bucketByTime(1, TimeUnit.DAYS)
+                        .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                        .build();
+        // [END build_read_data_request]
+
+        return readRequest;
+    }
+
+    public void cumulateCalories(DataReadResponse dataReadResult) {
+        week_expended_calories = 0;
+        if (dataReadResult.getBuckets().size() > 0) {
+            for (Bucket bucket: dataReadResult.getBuckets()) {
+                List<DataSet> dataSets = bucket.getDataSets();
+                for (DataSet dataSet: dataSets) {
+                    week_expended_calories += dataSet.getDataPoints().get(0).getValue(Field.FIELD_CALORIES).asFloat();
+                }
+            }
+        }
+        Log.i(TAG, "week_calories: " + String.valueOf(week_step_count));
+    }
+
+    public void cumulateStepCount(DataReadResponse dataReadResult) {
+        week_step_count = 0;
+        if (dataReadResult.getBuckets().size() > 0) {
+            for (Bucket bucket: dataReadResult.getBuckets()) {
+                List<DataSet> dataSets = bucket.getDataSets();
+                for (DataSet dataSet: dataSets) {
+                    week_step_count += dataSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
+                }
+            }
+        }
+        Log.i(TAG, "week_step_count: " + String.valueOf(week_step_count));
+    }
+
+
+
+    // for testing
+    public static void printData(DataReadResponse dataReadResult) {
+        // [START parse_read_data_result]
+        // If the DataReadRequest object specified aggregated data, dataReadResult will be returned
+        // as buckets containing DataSets, instead of just DataSets.
+        if (dataReadResult.getBuckets().size() > 0) {
+            Log.i(
+                    TAG, "Number of returned buckets of DataSets is: " + dataReadResult.getBuckets().size());
+            for (Bucket bucket : dataReadResult.getBuckets()) {
+                List<DataSet> dataSets = bucket.getDataSets();
+                for (DataSet dataSet : dataSets) {
+                    dumpDataSet(dataSet);
+                }
+            }
+        } else if (dataReadResult.getDataSets().size() > 0) {
+            Log.i(TAG, "Number of returned DataSets is: " + dataReadResult.getDataSets().size());
+            for (DataSet dataSet : dataReadResult.getDataSets()) {
+                dumpDataSet(dataSet);
+            }
+        }
+        // [END parse_read_data_result]
+    }
+
+
+    private static void dumpDataSet(DataSet dataSet) {
+        Log.i(TAG, "Data returned for Data type: " + dataSet.getDataType().getName());
+        DateFormat dateFormat = DateFormat.getTimeInstance();
+
+        for (DataPoint dp : dataSet.getDataPoints()) {
+            Log.i(TAG, "Data point:");
+            Log.i(TAG, "\tType: " + dp.getDataType().getName());
+            Log.i(TAG, "\tStart: " + dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+            Log.i(TAG, "\tEnd: " + dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)));
+            for (Field field : dp.getDataType().getFields()) {
+                Log.i(TAG, "\tField: " + field.getName() + " Value: " + dp.getValue(field));
+            }
+        }
     }
 }
